d92f1765dc8f7838c3a0480573fa984acfb4fc07
Mohit Bansal <simranhaidar91@gmail.com>
2025-05-01 16:15:00 +0530

    Add location fields to User model and GeoJSON index

--- models/User.js
+++ models/User.js
@@ -1,13 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  location: {
-    latitude: Number,
-    longitude: Number
+  name: {
+    type: String,
+    required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number], // [longitude, latitude]
+      default: [0, 0],
+    },
+  },
+  online: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
 });
 
-module.exports = mongoose.model("User", userSchema);
+// Create a 2dsphere index for location-based queries
+userSchema.index({ location: '2dsphere' });
+
+module.exports = mongoose.model('User', userSchema);
